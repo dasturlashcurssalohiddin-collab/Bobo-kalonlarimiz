49f0238f37bc0539141e4cbb11045c5b412b0d41
dasturlashcurssalohiddin-collab <dasturlashcurssalohiddin@gmail.com>
2026-04-07 09:39:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.post("/send", async (req, res) => {
     }
 });
 
-app.listen(3000, ()=>{
-    console.log("Server ishlayapti: http://localhost:3000");
+// ===== PORT sozlash =====
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server ishlayapti: http://localhost:${PORT}`);
 });
